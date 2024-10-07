f0ca839dec9b4e0f31612529a43e946b763e8737
Shivam Dubey <shivam.2000shivu@gmail.com>
2024-10-08 01:07:46 +0530

    Changing the config file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URI); // No need for options
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
-        process.exit(1);
+        process.exit(1); // Exit process with failure
     }
 };
+
 module.exports = connectDB;
